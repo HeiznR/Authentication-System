e194372971754ebcdf66851605578f953fb41ab7
LilHeizy <vishnyakov98@gmail.com>
2024-07-22 23:08:37 +0200

    apollo client for next.js configured; nginx service for front-back communication added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { getClient } from "@/config/apollo-server";
+import { gql } from "@apollo/client";
 
 export default function Home() {
+  const getUsers = gql`
+    query {
+      getUsers {
+        id
+        userName
+        name
+        surname
+        email
+        password
+      }
+    }
+  `;
+
+  getClient()
+    .query({ query: getUsers })
+    .then((data) => console.log("data", data));
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {"test12346"}
+      {"test"}
     </main>
   );
 }
